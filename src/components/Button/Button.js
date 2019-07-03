1f263a273cd691ff4463c29aea504a58921be3a0
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-02 20:04:15 -0600

    Initial test component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Button = () => {
+  return <button>Oh boy</button>;
+}
+
+Button.defaultProps = {};
+Button.propTypes = {};
+
+export default Button
