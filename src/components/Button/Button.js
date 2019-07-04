985cdb1e7c504ae1889ccbf5fa06d48d4b3bddd4
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-04 13:56:35 -0600

    Formatting

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,55 +3,55 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const Button = props => {
-	const className = classNames(
-		'rcu-button',
-		{
-			[`is-${props.color}`]: props.color,
-			[`is-${props.size}`]: props.size,
-			'is-block': props.block
-		}
-	);
+  const className = classNames(
+    'rcu-button',
+    {
+      [`is-${props.color}`]: props.color,
+      [`is-${props.size}`]: props.size,
+      'is-block': props.block
+    }
+  );
 
-	const componentProps = {
-		className,
-		disabled: props.disabled,
-		onClick: props.onClick,
-		type: props.type
-	}
+  const componentProps = {
+    className,
+    disabled: props.disabled,
+    onClick: props.onClick,
+    type: props.type
+  }
 
-	return (
-		<button {...componentProps}>
-			{props.children}
-		</button>
-	);
+  return (
+    <button {...componentProps}>
+      {props.children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
-	children: '',
-	color: 'default',
-	size: 'normal'
+  children: '',
+  color: 'default',
+  size: 'normal'
 };
 Button.propTypes = {
-	block: PropTypes.bool,
-	children: PropTypes.oneOfType([
-		PropTypes.node,
-		PropTypes.string,
-		PropTypes.number
-	]),
-	disabled: PropTypes.bool,
-	color: PropTypes.oneOf([
-		'default',
-		'danger',
-		'success',
-		'warning'
-	]),
-	onClick: PropTypes.func,
-	size: PropTypes.oneOf([
-		'small',
-		'normal',
-		'large'
-	]),
-	type: PropTypes.string
+  block: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  disabled: PropTypes.bool,
+  color: PropTypes.oneOf([
+    'default',
+    'danger',
+    'success',
+    'warning'
+  ]),
+  onClick: PropTypes.func,
+  size: PropTypes.oneOf([
+    'small',
+    'normal',
+    'large'
+  ]),
+  type: PropTypes.string
 };
 
 export default Button
