d155342e6e24200fa4ee9777cb637fe739c354b4
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-07-03 19:47:42 -0600

    Base button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,57 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Button = () => {
-  return <button>Oh boy</button>;
+const Button = props => {
+	const className = classNames(
+		'rcu-button',
+		{
+			[`is-${props.color}`]: props.color,
+			[`is-${props.size}`]: props.size,
+			'is-block': props.block
+		}
+	);
+
+	const componentProps = {
+		className,
+		disabled: props.disabled,
+		onClick: props.onClick,
+		type: props.type
+	}
+
+	return (
+		<button {...componentProps}>
+			{props.children}
+		</button>
+	);
 }
 
-Button.defaultProps = {};
-Button.propTypes = {};
+Button.defaultProps = {
+	children: '',
+	color: 'default',
+	size: 'normal'
+};
+Button.propTypes = {
+	block: PropTypes.bool,
+	children: PropTypes.oneOfType([
+		PropTypes.node,
+		PropTypes.string,
+		PropTypes.number
+	]),
+	disabled: PropTypes.bool,
+	color: PropTypes.oneOf([
+		'default',
+		'danger',
+		'success',
+		'warning'
+	]),
+	onClick: PropTypes.func,
+	size: PropTypes.oneOf([
+		'small',
+		'normal',
+		'large'
+	]),
+	type: PropTypes.string
+};
 
 export default Button
